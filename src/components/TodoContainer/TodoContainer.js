a143c95636827efe3cba3a7c9efbc204c8226347
Harsh Rana <hr23232323@gmail.com>
2018-12-17 12:17:26 -0500

    Todo Container implemented

---
+++ src/components/TodoContainer/TodoContainer.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+import TodoList from '../TodoList/TodoList';
+import TodoSearch from '../TodoSearch/TodoSearch';
+
+
+class TodoContainer extends Component{
+  constructor(props){
+    super(props);
+    this.todoStore = props.stors.todoStore;
+    this.search = this.serach.bind(this);
+    this.relaod = this.reload.bind(this);
+
+    this.query = null;
+    this.state = {
+      todos:[]
+    };
+  }
+
+  componentDidMount(){
+    this.todoStore.onChange(this.reload);
+    this.todoStore.fetch();
+  }
+
+  reload(){
+    const todos = this.todoStore.getBy(this.query);
+    this.setState({todos});
+  }
+
+  search(query){
+    this.query = query;
+    this.reload();
+  }
+
+  render(){
+    return(
+      <div>
+        <TodoSearch onSearch = {this.search}/>
+        <TodoList todos = {this.state.todos}/>
+      </div>
+    );
+  }
+
+}
+
+export default TodoContainer
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TodoListItem from '../TodoListItem/TodoListItem';
+
 
 function TodoList(props){
   function renderTodoItem(todo){
